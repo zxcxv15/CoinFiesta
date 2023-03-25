78df2b40335920ab0d25f02bbe67a93e90d6a578
DESKTOP-C0UJHQD\zxcxv <zxcxv15@daum.net>
2023-03-25 17:38:04 +0900

    최초 생성

---
+++ src/main/java/com/CoinFiesta/CoinFiesta/CoinFiestaApplication.java
@@ -0,0 +1,13 @@
+package com.CoinFiesta.CoinFiesta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinFiestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinFiestaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CoinFiesta/CoinFiesta/CoinFiestaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CoinFiesta.CoinFiesta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinFiestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
