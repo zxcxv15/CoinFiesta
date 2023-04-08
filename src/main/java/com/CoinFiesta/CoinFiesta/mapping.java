aa401ed839e75bed7315f5462ad2021eb224eabf
DESKTOP-7G7JS9C\USER <zxcxv15@daum.net>
2023-04-08 16:42:18 +0900

    db 연결

--- src/main/java/com/CoinFiesta/CoinFiesta/mapping.java
+++ src/main/java/com/CoinFiesta/CoinFiesta/mapping.java
@@ -1,10 +1,17 @@
 package com.CoinFiesta.CoinFiesta;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @Controller
 public class mapping {
+	
+	@Autowired
+	private testRep rep;
 	
 	@GetMapping("/")
 	public String getMethod() {
@@ -16,5 +23,17 @@ public class mapping {
 	public String hoga() {
 		return "hoga.html";
 	}
+	
+	@GetMapping("/test")
+	public String test() {
+		try {
+			System.out.println(rep.test());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "test.html";
+	}
+	
 	
 }
---
+++ src/main/java/com/CoinFiesta/CoinFiesta/testRep.java
@@ -0,0 +1,9 @@
+package com.CoinFiesta.CoinFiesta;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface testRep {
+
+	public int test() throws Exception;
+}
